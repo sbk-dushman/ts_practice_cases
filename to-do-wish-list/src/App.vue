4b36471b3ed109fcb7671e86c5c8167b5a341393
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2025-04-06 21:13:45 +0500

    intial commit

---
+++ to-do-wish-list/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import AppCard from './components/AppCard.vue';
+</script>
+
+<template>
+  <header>
+<h1 class="title">To do list</h1>
+  </header>
+  <main>
+    <div class="container">
+     <ul>
+      <AppCard/>
+     </ul>
+    </div>
+   
+  </main>
+</template>
+
+<style scoped>
+@import './assets/base.css';
+
+</style>
---
+++ to-do-wish-list/src/components/AppCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <li class="card">1</li>
+</template>
+
+<style scoped>
+ .card{
+  background-color:var(--white);
+  width: 300px;
+  height: 150px;
+  padding: 20px;
+  border-radius: 20px;
+} 
+</style>
