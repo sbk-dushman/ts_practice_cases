48f276a1bacd9fe9d0c2bfc0579dc55ada350a9d
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2025-04-29 12:45:27 +0500

    m2 almost front 26.04

--- to-do-wish-list/src/App.vue
+++ to-do-wish-list/src/App.vue
@@ -1,16 +1,18 @@
 <!-- /* todo 
   
-фронт 
-закрыть цвет и размер кнопок 
-кнопка закрытия 
++фронт 
++закрыть цвет и размер кнопок 
++кнопка закрытия 
  вынос в отдельный компонент 
-и тд
+обработка кликов
+стейт
 
 */ -->
 
 <template>
       <div class="modal__container">
       <div class="modal">
+        <button class="modal__close-btn">&#10006;</button>
       <h3 class="modal__title">Редактирование курса </h3>
       <form class="form" action="">
 
@@ -25,8 +27,8 @@
         </label>
         
         <div class="form__btn-group">
-          <button type="submit">сохранить</button>
-          <button>Отмена</button>
+          <button class="form__btn-save" type="submit">Cохранить</button>
+          <button class="form__btn-cancel">Отмена</button>
         </div>
 
         </form>
@@ -73,7 +75,7 @@ const cards = ref([
 @import './assets/base.css';
 .modal__title{
   text-align: center;
-  margin-bottom: 25px;
+  margin-bottom: 26px;
   font-weight: 900;
 }
  .modal__container{
@@ -94,22 +96,43 @@ const cards = ref([
   flex-direction: column;
   padding: 20px;
   padding-top: 42px;
+  position: relative;
  }
  .form{
   display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 10px;
-    height: 100%;
+  flex-direction: column;
+  align-items: center;
+  gap: 17px;
+  height: 100%;
  }
  .input-title{
   display: block;
+  font-size: 15px;
+  
  }
- .form__name,.form__description{
+ .form__name,.form__description,.form__btn-save,.form__btn-cancel{
   height: 15px;
  }
  .form__btn-group{
-  margin-top: 40px;
+  margin-top: 26px;
+  display: flex;
+  gap: 10px;
+ }
+ .form__btn-save,.form__btn-cancel{
+  width: 145px;
+  font-size: 10px;
+ }
+ .form__btn-save{
+  background-color:var(--done);
+ }
+ .modal__close-btn{
+  position: absolute;
+  position: absolute;
+    right: 18px;
+    top: 18px;
+    border:none;
+    background:none;
+    
  }
 
 </style>
