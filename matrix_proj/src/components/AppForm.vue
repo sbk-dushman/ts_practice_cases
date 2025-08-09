7cffce0dd0accb8e757c177e6e43fcb91b41bbe1
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2025-08-10 00:39:08 +0500

    matrix forn modal part 1

--- matrix_proj/src/components/AppForm.vue
+++ matrix_proj/src/components/AppForm.vue
@@ -2,21 +2,21 @@
 <template>
 1S
 
-   <form action class="task-form">
-    <h1 class="task-form__title">Новая задача {{title}} </h1>
-   <input v-model.title="title" placeholder="task" type="text" class="task-form__input-task">
+   <form @submit="" class="task-form">
+    <h1 class="task-form__title">Новая задача {{newTask.title}} </h1>
+   <input v-model.title="newTask.title" placeholder="task" type="text" class="task-form__input-task">
 
         <div class="task-form__priority-time">
               <label for="task-form__input-time">  время {{time}}
-        <select v-model.time="time" class="task-form__input-time"  name="" id="task-form__input-time">
+        <select v-model="newTask.time" class="task-form__input-time"  name="" id="task-form__input-time">
           <option selected value=10>10мин</option>
           <option value=25>25мин</option>
           <option value=60>1ч</option>
           <option value=120>2ч</option>
         </select>
             </label>
-          <label for="task-form__input-priority"> Группа {{ priority }}
-            <select v-model.priority="priority" class="task-form__input-priority" name="" id="task-form__input-priority">
+          <label for="task-form__input-priority"> Группа {{  }}
+            <select v-model="newTask.priority" class="task-form__input-priority" name="" id="task-form__input-priority">
                 <option selected value=1>a</option>
                 <option value=2>b</option>
                 <option value=3>c</option>
@@ -26,25 +26,48 @@
 
         </div>
 
-        <button type="submit" @click.prevent="addCard(task)" class="task-form__btn">add</button>
+        <button type="submit" @click.prevent="addCard()" class="task-form__btn">add</button>
    </form>
 </template>
 <script setup lang="ts">
 import { useTasksStore } from '@/stores/task';
 import { ref } from 'vue';
- const task= ref({
+// const title = defineModel('title');
+// const priority = defineModel('priority');
+// const time = defineModel('time');
+// const taskStore= useTasksStore();
+
+// прописать пропсы нормально
+// const props= defineProps({
+//   title:{
+//     type:String,
+
+//   },
+//     priority:{
+//       type:Number
+//   },
+//     time:{
+//     type:Number
+//   }
+// })
+ const newTask= ref({
  title:'',
- priority:1,
+ priority:0,
  time:25
  })
-const title = defineModel('title');
-const priority = defineModel('priority');
-const time = defineModel('time');
+
 const addCard =()=>{
   alert("mox")
+console.log(newTask.value);
+
+//  taskStore.addTask()
+
 }
-const taskStore= useTasksStore();
-taskStore
+
+
+
+
+
 
 </script>
 
