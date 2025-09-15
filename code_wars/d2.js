f993541ba75f1d1ee120dee5566a39c03059dbbd
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2025-09-15 22:26:33 +0500

    сode wars d2

---
+++ code_wars/d2.js
@@ -0,0 +1,10 @@
+function sum (numbers) {
+  let result = 0
+     numbers.forEach(element =>  result = result + element );
+  return result;
+}
+sum([1, 5.2, 4, 0, -1]);
+// разобрать решение из каты 
+//https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/javascript
+
+
