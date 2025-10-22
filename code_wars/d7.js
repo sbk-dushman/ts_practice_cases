50e4ffa2000fd605d1e10c23c855ff92cc97378f
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2025-10-22 23:35:36 +0500

    d7

---
+++ code_wars/d7.js
@@ -0,0 +1,17 @@
+
+// 22.10.25 
+// Write a function "greet" that returns "hello world!"
+const greet= () => "hello world!";
+https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+// task 2 доделать 23.10.25
+https://www.codewars.com/kata/55f2b110f61eb01779000053/train/javascript
+function getSum(a, b)
+{   let courent_A = a;
+    let courent_B = b;
+ if(a<b){
+   while (courent_A < courent_B) {
+    courent_A+ 1;
+   }
+ }
+ 
+}
---
+++ code_wars/d8.js
@@ -0,0 +1,5 @@
+https://www.codewars.com/kata/580f6cbcfbf2bec47c000511/train/javascript
+// 12.10.25
+function createGrid(m, n, position) {
+
+}
