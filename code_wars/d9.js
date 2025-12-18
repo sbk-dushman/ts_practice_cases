4c7d6eebd180a7d17ffd74fbc13970c456172e12
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2025-12-18 23:00:58 +0500

    сode wars

---
+++ code_wars/d9.js
@@ -0,0 +1,15 @@
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+// 18.12.25
+function numberToString(num) {
+     console.log(num+=""); 
+    return num+="";
+}
+// numberToString(1);
+//2  начнал 18.12.12 https://www.codewars.com/kata/56b1f01c247c01db92000076/train/javascript
+function DoubleChar(char){
+  
+  console.log(char.split("").map((i)=>console.log(i)
+  ));
+  
+}
+DoubleChar('123456');
